95603ac081a4e4a96d76d041f1d4eaa9cca11147
rohitjainfsl <rohit@fullstacklearning.com>
2023-02-06 18:22:21 +0530

    added axios

---
+++ src/shoppingCart/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='static-page about'>
+        <h2>About Ecommerce</h2>
+        <p></p>
+    </div>
+  )
+}
+
+export default About
