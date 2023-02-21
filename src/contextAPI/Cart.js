c4934aed83c63a7f5af5dc1110cd878ddaeecb01
rohitjainfsl <rohit@fullstacklearning.com>
2023-02-22 00:55:26 +0530

    ecommerce with context

---
+++ src/contextAPI/Cart.js
@@ -0,0 +1,43 @@
+import { Output } from "@mui/icons-material";
+import React, { useContext, useEffect, useState } from "react";
+import { cartContext } from "./cartContext";
+
+function Cart() {
+  const [productsToShow, setProductsToShow] = useState(null);
+  const { cart } = useContext(cartContext);
+
+  useEffect(() => {
+    if (localStorage.getItem("cartItems") !== null) {
+      const temp = JSON.parse(localStorage.getItem("cartItems"));
+      setProductsToShow(temp.map((item) => item));
+    }
+  }, []);
+  console.log(productsToShow);
+
+  return (
+    <div className="cart">
+      <h1>Cart</h1>
+      {productsToShow !== null ? (
+        <div className="cartItems">
+          {productsToShow.map((product) => (
+            <div className="cartItem">
+              <img src={product.image}></img>
+              <p>
+                <span>
+                  {product.title.length > 50
+                    ? product.title.slice(0, 50) + "..."
+                    : product.title}
+                </span>
+                <span>INR {Math.round(product.price * 85)}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Your Cart is empty. Shop something</p>
+      )}
+    </div>
+  );
+}
+
+export default Cart;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./shoppingCart/Home";
+// import Home from "./shoppingCart/Home";
+// import Counter from "./features/counter/Counter";
+// import Store from "./app/Store";
+// import { Provider } from "react-redux";
+import App from './contextAPI/App'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Home />);
+root.render(
+  // <Provider store={Store}>
+    <App />
+  // </Provider>
+);
